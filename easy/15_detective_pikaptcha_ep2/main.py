cc30131750b1768b0399e271fcc4e89c90e52775
Philippe BAUCOUR <philippe.baucour@gmail.com>
2025-02-05 11:42:04 +0100

    detective_pikaptcha_ep2 is done

--- easy/15_detective_pikaptcha_ep2/main.py
+++ easy/15_detective_pikaptcha_ep2/main.py
@@ -1,5 +1,5 @@
 # https://www.codingame.com/ide/puzzle/detective-pikaptcha-ep2
-
+# see https://bradleycarey.com/posts/2012-08-15-maze-solving-algorithms-wall-follower/
 
 # -----------------------------------------------------------------------------
 RedirectIOtoFile = True
@@ -8,10 +8,9 @@ if RedirectIOtoFile:
     import os
     from pathlib import Path
 
-    k_input = "input_one_based.txt"
+    k_input = "input.txt"
     os.chdir(Path(__file__).parent)
     sys.stdin = open(k_input, "r")
-
 
 # # -----------------------------------------------------------------------------
 # # To debug: print("Debug messages...", file=sys.stderr, flush=True)
@@ -21,18 +20,83 @@ if RedirectIOtoFile:
 # print(f"Execution time: {(end_time - start_time) * 1_000_000 :.2f} µs")
 
 
-# -----------------------------------------------------------------------------
+# -------------------------------------
+def walk(y, x, dir):
+    # Get the priorities according the current direction
+    prios = priorities[direction_indices.get(dir)]
+    for dir in prios:
+        match (dir):
+            case ">":
+                if x + 1 < w and map[y][x + 1] != "#":
+                    map[y][x] = str(int(map[y][x]) + 1)  # if speed becomes an issue keep int in the map
+                    x += 1
+                    break  # the for loop
+            case "v":
+                if y + 1 < h and map[y + 1][x] != "#":
+                    map[y][x] = str(int(map[y][x]) + 1)  # if speed becomes an issue keep int in the map
+                    y += 1
+                    break
+            case "<":
+                if x - 1 >= 0 and map[y][x - 1] != "#":
+                    map[y][x] = str(int(map[y][x]) + 1)  # if speed becomes an issue keep int in the map
+                    x -= 1
+                    break
+            case "^":
+                if y - 1 >= 0 and map[y - 1][x] != "#":
+                    map[y][x] = str(int(map[y][x]) + 1)  # if speed becomes an issue keep int in the map
+                    y -= 1
+                    break
+    return y, x, dir
 
 
-width, height = [int(i) for i in input().split()]
-for i in range(height):
-    line = input()
+# -------------------------------------
+k_North = "^"
+k_South = "v"
+k_East = ">"
+k_West = "<"
+
+# direction_mapping = {"^": k_North, "v": k_South, ">": k_East, "<": k_West}
+direction_indices = {"^": 0, "v": 1, ">": 2, "<": 3}
+
+# According the current direction, the priorities are different
+# 0: North, 1: East, 2: South, 3: West
+priorities = []
+priorities.append([k_West, k_North, k_East, k_South])
+priorities.append([k_East, k_South, k_West, k_North])
+priorities.append([k_North, k_East, k_South, k_West])
+priorities.append([k_South, k_West, k_North, k_East])
+
+w, h = [int(i) for i in input().split()]
+
+# fill the map, update insertion point and initial direction
+map = []
+for y in range(h):
+    line = list(input())
+    map.append(line)
+    for x, c in enumerate(line):
+        if c in ("^", "v", ">", "<"):
+            dir = c
+            x0 = x
+            y0 = y
+            map[y][x] = "0"
 
 side = input()
+# if side is R, exchange columns 0 and 2 in priorities
+if side == "R":
+    for l in priorities:
+        l[0], l[2] = l[2], l[0]
 
-for i in range(height):
 
-    print("#####")
+x = x0
+y = y0
+while True:
+    y, x, dir = walk(y, x, dir)
+    # print(f"{y} {x} {dir}")
+    if x == x0 and y == y0:
+        break
+
+for y in range(h):
+    print("".join(map[y]))
 
 
 # -----------------------------------------------------------------------------
