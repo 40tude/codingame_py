759e6e87ac9c1dabfe98ff2adcfccbabde750cf8
Philippe BAUCOUR <philippe.baucour@gmail.com>
2025-02-12 19:25:14 +0100

    start working on brainfuck

--- medium/08_what_the_brainfuck/main.py
+++ medium/08_what_the_brainfuck/main.py
@@ -41,11 +41,148 @@ if RedirectIOtoFile:
 
 
 # -----------------------------------------------------------------------------
-line_count, array_size, n_inputs = map(int, input().split())
-prog_lines = [input() for _ in range(line_count)]
-inputs = [int(input()) for _ in range(n_inputs)]
-array = [0] * array_size
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+import sys
 
+
+# -------------------------------------
+def preprocessor():
+    line_out = ""
+    for line in prog_lines:
+        for op_code in line:
+            if op_code in ops:
+                line_out += op_code
+    return line_out
+
+
+# -------------------------------------
+def parser(line):
+    stack = []
+    for op_code in line:
+        if op_code == "[":
+            stack.append(op_code)
+        elif op_code == "]":
+            if not stack:
+                return
+            stack.pop()
+    if stack:
+        print("SYNTAX ERROR")
+        sys.exit()
+
+
+# -------------------------------------
+def ptr_check(addr):
+    if addr < 0 or addr > ram_size - 1:
+        print("POINTER OUT OF BOUNDS")
+        sys.exit()
+
+
+def val_check(addr):
+    if addr < 0 or addr > 255:
+        print("INCORRECT VALUE")
+        sys.exit()
+
+
+# -------------------------------------
+def val_inc(addr, ip):
+    ram[addr] = ram[addr] + 1
+    val_check(ram[addr])
+    return addr, (ip := ip + 1)
+
+
+def val_dec(addr, ip):
+    ram[addr] = ram[addr] - 1
+    val_check(ram[addr])
+    return addr, (ip := ip + 1)
+
+
+# -------------------------------------
+def addr_inc(addr, ip):
+    # addr += 1
+    ptr_check(addr := addr + 1)
+    return addr, (ip := ip + 1)
+
+
+def addr_dec(addr, ip):
+    # addr -= 1
+    ptr_check(addr := addr - 1)
+    return addr, (ip := ip + 1)
+
+
+# -------------------------------------
+def STO(addr, ip):
+    ram[addr] = params.pop(0)
+    val_check(ram[addr])
+    return addr, (ip := ip + 1)
+
+
+# -------------------------------------
+def output(addr, ip):
+    print(chr(ram[addr]), end="")
+    return addr, (ip := ip + 1)
+
+
+# -------------------------------------
+def jump_fwd(addr, ip):
+    stack.append(ip)
+    if ram[addr] == 0:
+        ip = stack.pop()
+    return addr, (ip := ip + 1)
+
+
+# -------------------------------------
+def jump_bck(addr, ip):
+    if ram[addr] != 0:
+        ip = stack[-1]
+    return addr, (ip := ip + 1)
+
+
+# > increment the pointer position.
+# < decrement the pointer position.
+# + increment the value of the cell the pointer is pointing to.
+# - decrement the value of the cell the pointer is pointing to.
+# . output the value of the pointed cell, interpreting it as an ASCII value.
+# , accept a positive one byte integer as input and store it in the pointed cell.
+# [ jump to the instruction after the corresponding ] if the pointed cell's value is 0. Can be NESTED
+# ] go back to the instruction after the corresponding [ if the pointed cell's value is different from 0.
+ops = {
+    ">": addr_inc,
+    "<": addr_dec,
+    "+": val_inc,
+    "-": val_dec,
+    ".": output,
+    ",": STO,
+    "[": jump_fwd,
+    "]": jump_bck,
+}
+
+# -------------------------------------
+line_count, ram_size, n_inputs = map(int, input().split())
+prog_lines = [input() for _ in range(line_count)]
+params = [int(input()) for _ in range(n_inputs)]
+
+line = preprocessor()  # Remove comments... Flatten the source code on a single line
+parser(line)  # Check for syntax errors
+
+ip = 0  # Instruction Pointer
+addr = 0  # Pointer to the current cell in the RAM
+ram = [0] * ram_size
+stack: list[int] = []
+
+while ip < len(line):
+    op_code = line[ip]
+    if op_code in ops:
+        addr, ip = ops[op_code](addr, ip)
+    else:
+        ip += 1
 
 # -----------------------------------------------------------------------------
 if RedirectIOtoFile:
